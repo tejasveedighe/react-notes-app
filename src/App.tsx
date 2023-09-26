b6d2a933fcced04ca9decff490fd874121a1a4c9
tejasveedighe <tejasveedighe@gmail.com>
2023-09-26 21:47:49 +0530

    added tailwind, created a basic page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classNames from "classnames";
+import styles from "./App.module.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<div className={classNames(styles.parent, "border-8")}>
+			<header className="text-3xl font-bold text-center text-cyan-400">
+				Notes App
+			</header>
+			<div className="notes mt-12 flex gap-2">
+				<div className={styles.note}>Some hsdasdasdasdasdasdasdasdsdlnote</div>
+				<div className={styles.note}>Some note</div>
+				<div className={styles.note}>Some note</div>
+			</div>
+		</div>
+	);
 }
-
-export default App;
