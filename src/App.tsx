1cbbff01bb89e154ca8c40ce2676141e1b2e3947
tejasveedighe <tejasveedighe@gmail.com>
2023-09-27 22:10:48 +0530

    OAuth
    Created Route to login and redirect to home if the login true

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,33 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Notes from "./pages/home-page/Notes";
 import Note from "./pages/note/Note";
+import { Auth } from "./pages/auth/Auth";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/firebase";
 
 export default function App() {
+	const [hasSignedIn, setHasSignedIn] = useState<boolean>(false);
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				console.log(user);
+				setHasSignedIn((prev) => !prev);
+			} else {
+				console.log("signed out");
+			}
+		});
+	}, []);
 	return (
 		<Routes>
-			<Route element={<Layout />}>
-				<Route index path="/" element={<Notes />} />
-				<Route path="/note/:id" element={<Note />} />
-			</Route>
+			{hasSignedIn ? (
+				<Route path="/" element={<Auth />} />
+			) : (
+				<Route element={<Layout />}>
+					<Route index path="/" element={<Notes />} />
+					<Route path="/note/:id" element={<Note />} />
+				</Route>
+			)}
 		</Routes>
 	);
 }
---
+++ src/pages/auth/Auth.tsx
@@ -0,0 +1,124 @@
+import {
+	createUserWithEmailAndPassword,
+	signInWithPopup,
+	signOut,
+} from "firebase/auth";
+import { useCallback, useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { auth, googleProvider } from "../../firebase/firebase";
+
+export const Auth = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const signIn = async () => {
+		try {
+			await createUserWithEmailAndPassword(auth, email, password);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+	const signInWithGoogle = async () => {
+		try {
+			await signInWithPopup(auth, googleProvider);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+	const logOut = async () => {
+		try {
+			await signOut(auth);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	const handleEmailChange = useCallback(
+		(e: React.FormEvent<HTMLInputElement>) => {
+			setEmail(e.currentTarget.value);
+		},
+		[]
+	);
+	const handlePasswordChange = useCallback(
+		(e: React.FormEvent<HTMLInputElement>) => {
+			setPassword(e.currentTarget.value);
+		},
+		[]
+	);
+
+	return (
+		<section className="bg-gray-50 dark:bg-gray-900">
+			<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+				<a
+					href="#"
+					className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+				>
+					<img
+						className="w-8 h-8 mr-2"
+						src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+						alt="logo"
+					/>
+					Notes
+				</a>
+				<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+					<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+						<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+							Welcome
+						</h1>
+						<form className="space-y-4 md:space-y-6" action="#">
+							<div>
+								<label
+									htmlFor="email"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+								>
+									Your email
+								</label>
+								<input
+									type="email"
+									name="email"
+									id="email"
+									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									placeholder="name@company.com"
+									required
+									value={email}
+									onChange={handleEmailChange}
+								/>
+							</div>
+							<div>
+								<label
+									htmlFor="password"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+								>
+									Password
+								</label>
+								<input
+									type="password"
+									name="password"
+									id="password"
+									placeholder="••••••••"
+									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									required
+									value={password}
+									onChange={handlePasswordChange}
+								/>
+							</div>
+
+							<button
+								type="submit"
+								className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+								onClick={signIn}
+							>
+								Log In
+							</button>
+							<button
+								className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 flex justify-center items-center"
+								onClick={signInWithGoogle}
+							>
+								<FcGoogle className="mt-1 mr-4" /> Sign Up With Google
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
