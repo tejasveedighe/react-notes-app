73e190ba2f90835d4e436159098149ca1f35f304
tejasveedighe <tejasveedighe@gmail.com>
2023-10-24 19:57:13 +0530

    removed unsed code

--- src/pages/add-note/AddNote.tsx
+++ src/pages/add-note/AddNote.tsx
@@ -1,13 +1,13 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { addDoc, collection } from "firebase/firestore";
 import { useCallback, useEffect, useState } from "react";
-import { IconContext } from "react-icons";
-import { TiTick } from "react-icons/ti";
 import { useNavigate } from "react-router-dom";
+import Input from "../../components/Input/Input";
+import SubmitButton from "../../components/SubmitButton/SubmitButton";
 import { auth, db } from "../../firebase/firebase";
 import { notify } from "../../helper";
-import styles from "./AddNote.module.css";
 import { NoteType } from "../../types/Notes";
+import styles from "./AddNote.module.css";
 
 export default function AddNote() {
 	const [uid, setUid] = useState<string>("");
@@ -46,16 +46,14 @@ export default function AddNote() {
 	return (
 		<div className="min-h-full mt-5 relative">
 			<div className={styles.inputsContainer}>
-				<input
+				<Input
 					required
 					autoFocus
-					className={styles.titleInput}
 					placeholder="Title.."
 					onChange={(e) => setTitle(e.target.value)}
 					value={title}
 				/>
-				<input
-					className={styles.categoryInput}
+				<Input
 					placeholder="Category of Note"
 					onChange={(e) => setCategory(e.target.value)}
 					value={category}
@@ -68,18 +66,7 @@ export default function AddNote() {
 					rows={15}
 				/>
 			</div>
-			<button
-				onClick={handleSubmit}
-				className="fixed bottom-3 right-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
-			>
-				<IconContext.Provider
-					value={{
-						size: "1.5em",
-					}}
-				>
-					<TiTick />
-				</IconContext.Provider>
-			</button>
+			<SubmitButton handleSubmit={handleSubmit} />
 		</div>
 	);
 }
